909a0be4130002812abd7e72391a10df4c122295
Mikhail Kulyapin <kulyapin@corp.sputnik.ru>
2017-06-09 16:05:02 +0300

    add api folder

---
+++ api/json/0.1/schemas.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "auth": require("./schemas/auth.json"),
+    "patient": require("./schemas/patient.json"),
+    "practitioner": require("./schemas/practitioner.json"),
+    "organization": require("./schemas/organization.json"),
+    "profile": require("./schemas/profile.json"),
+    "refs": require("./schemas/refs.json"),
+    "chat": require("./schemas/chat.json"),
+    "emr": require("./schemas/emr.json"),
+    "feedback": require("./schemas/feedback.json")
+};
